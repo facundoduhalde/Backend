b0d448d23ef9f45e5cff9dd38e39c82d9c15fc0d
facundoduhalde <facundo.duhalde@gmail.com>
2022-04-18 20:01:44 -0300

    Desafio Clase 12 - Websockets - Backend - Camada 30930

---
+++ typescriptEjemplo/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hola");
+let var1 = 4;
+//var1="pepe";
+const sumar = (a, b) => a + b;
+console.log(sumar(6, 8));
+//console.log(sumar("pepe",8));
---
+++ typescriptEjemplo/src/index.ts
@@ -0,0 +1,10 @@
+console.log("Hola");
+
+let var1 : number = 4;
+
+//var1="pepe";
+
+const sumar = (a : number,b : number) => a+b
+
+console.log(sumar(6,8));
+//console.log(sumar("pepe",8));
