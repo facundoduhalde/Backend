620c68fd2a5b46043c8cae9995c2a183d189f468
facundoduhalde <facundo.duhalde@gmail.com>
2022-04-25 20:38:04 -0300

    Primer Entrega Proyecto Final - Backend - Camada 30930

---
+++ Primer Entrega/src/services/express.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+export const app = express();
+const PORT = process.env.PORT || 8080;
+export const server = app.listen(PORT, () => {
+    console.log(`Servidor HTTP escuchando en el puerto ${server.address().port}`);
+  });
+
+server.on("error", (error) => console.log(`Error en servidor: ${error}`));
+
+export default express;
